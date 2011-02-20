9a894df63c9b1fb8f08815f45347feeb6c79de5e
Eric Barr <eb@anhstudios.com>
2011-02-20 00:47:24 -0500

    Added the initial work for building tbb as an external project

---
+++ cmake/BuildExternalTBB.cmake
@@ -0,0 +1,26 @@
+# Build the TBB vendor library
+
+set(tbb_source "${CMAKE_CURRENT_SOURCE_DIR}/tbb")
+set(tbb_binary "${CMAKE_CURRENT_BINARY_DIR}/tbb_build")
+
+if(WIN32)
+	set(tbb_build msbuild "${CMAKE_CURRENT_SOURCE_DIR}/tbb/build/vc10/makefile.sln")
+else()
+	set(tbb_build make)
+endif()
+
+ExternalProject_Add(tbb
+	DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
+	GIT_REPOSITORY https://github.com/anhstudios/tbb.git
+	GIT_TAG 3.0.5-custom
+	UPDATE_COMMAND ""
+	SOURCE_DIR ${tbb_source}
+	BINARY_DIR ${tbb_binary}
+	CONFIGURE_COMMAND ""
+	BUILD_COMMAND ${tbb_build}
+	INSTALL_COMMAND ""
+)
+
+# Set the path to the built Boost for use by other projects
+set(TBB_INCLUDE_DIR "${tbb_source}" CACHE PATH "" FORCE)
+set(TBB_LIBRARY_DIRS "${tbb_binary}/lib" CACHE PATH "" FORCE)
