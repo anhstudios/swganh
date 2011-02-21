8106b92837c20fa5e9fcafe659fa8136632b6bf5
Eric Barr <eb@anhstudios.com>
2011-02-20 22:00:55 -0500

    Updated the configure command and set the appropriate path for the include and library directories.

--- cmake/BuildExternalGLog.cmake
+++ cmake/BuildExternalGLog.cmake
@@ -4,9 +4,14 @@ set(glog_source "${CMAKE_CURRENT_BINARY_DIR}/vendor/glog")
 set(glog_binary "${CMAKE_CURRENT_BINARY_DIR}/vendor/glog-build")
 
 if(WIN32)
+    set(glog_configure "")
 	set(glog_build msbuild "${CMAKE_CURRENT_BINARY_DIR}/vendor/glog/google-glog_vc10.sln")
 else()
+    set(glog_configure ${glog_source}/configure)
 	set(glog_build make)
+
+    set(GLog_INCLUDE_DIR "${glog_binary}/src" CACHE PATH "" FORCE)
+    set(GLog_LIBRARY_DIRS "${glog_binary}/src" CACHE PATH "" FORCE)
 endif()
 
 ExternalProject_Add(glog
@@ -15,11 +20,7 @@ ExternalProject_Add(glog
 	UPDATE_COMMAND ""
 	SOURCE_DIR ${glog_source}
 	BINARY_DIR ${glog_binary}
-	CONFIGURE_COMMAND ""
+	CONFIGURE_COMMAND ${glog_configure}
 	BUILD_COMMAND ${glog_build}
 	INSTALL_COMMAND ""
 )
-
-# Set the path to the built Boost for use by other projects
-set(GLog_INCLUDE_DIR "${glog_source}" CACHE PATH "" FORCE)
-set(GLog_LIBRARY_DIRS "${glog_binary}/lib" CACHE PATH "" FORCE)
