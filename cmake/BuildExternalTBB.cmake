fb1aa28c646b2a5e562d3c35fbab149436388d3a
Eric Barr <eb@anhstudios.com>
2011-02-20 22:01:13 -0500

    Set the appropriate include and library paths for unix builds.

--- cmake/BuildExternalTBB.cmake
+++ cmake/BuildExternalTBB.cmake
@@ -3,10 +3,14 @@
 set(tbb_source "${CMAKE_CURRENT_BINARY_DIR}/vendor/tbb")
 set(tbb_binary "${CMAKE_CURRENT_BINARY_DIR}/vendor/tbb-build")
 
+set(TBB_INCLUDE_DIR "${tbb_source}/include" CACHE PATH "" FORCE)
+
 if(WIN32)
 	set(tbb_build msbuild "${CMAKE_CURRENT_BINARY_DIR}/vendor/tbb/build/vc10/makefile.sln")
 else()
-	set(tbb_build make)
+	set(tbb_build make tbb_root=${tbb_source} tbb_build_dir=${tbb_binary}/lib tbb_build_prefix=gcc)
+
+    set(TBB_LIBRARY_DIRS "${tbb_binary}/lib" CACHE PATH "" FORCE)
 endif()
 
 ExternalProject_Add(tbb
@@ -14,12 +18,8 @@ ExternalProject_Add(tbb
 	GIT_TAG v3.0.5-custom
 	UPDATE_COMMAND ""
 	SOURCE_DIR ${tbb_source}
-	BINARY_DIR ${tbb_binary}
+	BINARY_DIR ${tbb_source}
 	CONFIGURE_COMMAND ""
 	BUILD_COMMAND ${tbb_build}
 	INSTALL_COMMAND ""
 )
-
-# Set the path to the built Boost for use by other projects
-set(TBB_INCLUDE_DIR "${tbb_source}" CACHE PATH "" FORCE)
-set(TBB_LIBRARY_DIRS "${tbb_binary}/lib" CACHE PATH "" FORCE)
