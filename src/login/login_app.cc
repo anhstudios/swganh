691fcf8e668d83717cb3c9d7e84d8ff987b266cf
Kyle Craviotto <kronos@swganh.org>
2011-05-11 15:17:42 -0700

    simplified login executable, should startup now

--- src/login/login_app.cc
+++ src/login/login_app.cc
@@ -29,12 +29,15 @@ LoginApp::LoginApp(int argc, char* argv[], list<string> config_files
 {
     auto startupListener = [&] (shared_ptr<EventInterface> incoming_event)-> bool {
         cout << "Login Application Startup" <<endl;
+        
         started_ = true;
         cout << "Login Application Started" << endl;
 
         return true;
     };
     auto processListener = [] (shared_ptr<EventInterface> incoming_event)-> bool {
+        // return galaxy status list every 5s
+        // heartbeat
         return true;
     };
     auto shutdownListener = [&] (shared_ptr<EventInterface> incoming_event)-> bool {
@@ -55,4 +58,49 @@ void LoginApp::onAddDefaultOptions_() {
 }
 void LoginApp::onRegisterApp_() {
 }
+
 } //namespace login
+using namespace login;
+int main(int argc, char* argv[])
+{
+    /// pre startup
+    // Initialize the google logging.
+    google::InitGoogleLogging(argv[0]);
+
+    #ifndef _WIN32
+        google::InstallFailureSignalHandler();
+    #endif
+
+    FLAGS_log_dir = "./logs";
+    FLAGS_stderrthreshold = 1;
+    setvbuf( stdout, NULL, _IONBF, 0);
+    // config files
+    list<string> config;
+    config.push_back("config/login.cfg");
+    shared_ptr<EventDispatcherInterface> dispatcher = make_shared<EventDispatcher>();
+    shared_ptr<ScriptingManagerInterface> scripter = make_shared<ScriptingManager>("scripts");
+    shared_ptr<Clock> clock = make_shared<Clock>();
+    auto services = make_shared<PlatformServices>();
+    // add services
+    services->addService("EventDispatcher", dispatcher);
+    services->addService("ScriptingManager", scripter);
+    services->addService("Clock", clock);
+    LoginApp app(argc, argv, config, services);
+    ///
+    app.startup();
+    boost::this_thread::sleep(boost::posix_time::milliseconds(5));
+    if (app.hasStarted()) {
+        while(true){
+            app.process();
+            if (app.kbHit())
+            {
+                if(cin.get() == 'q')
+                {
+                    app.shutdown();
+                    return 0;
+                }
+            }
+            boost::this_thread::sleep(boost::posix_time::milliseconds(1));
+        }
+    }
+}
