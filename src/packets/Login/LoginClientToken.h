7723f6ed5eabec86d045cd0a50bef4aa25bc0277
Dallin Wellington <dallin.wellington@gmail.com>
2011-05-20 07:04:02 -0700

    Login has been achieved! (albeit in a very fragile manner at the moment) Sending Data Message has been implimented and in a bandwidth efficient manner (which is to be expanded upon soon).

--- src/packets/Login/LoginClientToken.h
+++ src/packets/Login/LoginClientToken.h
@@ -9,19 +9,40 @@ struct LoginClientToken : public BasePacket
 {
     explicit LoginClientToken(std::shared_ptr<network::Session> session_  = nullptr)
         : BasePacket(session_, NONE){}
-    int32_t session_key;
+	anh::ByteBuffer session_key;
     int32_t account_id;
     int32_t station_id;
     std::string station_username;
 };
 
 class LoginClientTokenEvent : public anh::event_dispatcher::BasicEvent<LoginClientToken>{
-public:    
+public:
     LoginClientTokenEvent(std::shared_ptr<network::Session> session_  = nullptr) 
-        : anh::event_dispatcher::BasicEvent<LoginClientToken>("LoginClientToken"){}
+        : anh::event_dispatcher::BasicEvent<LoginClientToken>("LoginClientToken")
+	{ 
+		account_id = 0xDEADBABE;
+		station_id = 0xDDDDBBBB;
+		station_username = "username";
+		session_key.write<uint64_t>(0xDEADBABEDEADBABE); 
+	}
+
     virtual ~LoginClientTokenEvent() {}
+
+	void serialize(anh::ByteBuffer& buffer) {
+		buffer.write<uint16_t>(4);
+		buffer.write<uint32_t>(0xAAB296C6);
+		buffer.write<uint32_t>(session_key.size());
+		buffer.append(session_key);
+
+		buffer.write<uint32_t>(account_id);
+		buffer.write<uint32_t>(station_id);
+		buffer.write<std::string>(station_username);
+	}
+
     void deserialize(anh::ByteBuffer& buffer) {
-        session_key = buffer.read<int32_t>();
+		session_key = anh::ByteBuffer(buffer.data(), buffer.read<uint32_t>());
+		buffer.read_position(buffer.read_position() + session_key.size());
+
         account_id = buffer.read<int32_t>();
         station_id = buffer.read<int32_t>();
         station_username = buffer.read<std::string>();
