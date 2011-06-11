962a6b522ccc0cee67a62a8989186306b4c1af47
Eric Barr <eb@anhstudios.com>
2011-06-10 23:36:01 -0400

    Refactored the 2 existing tests into 3 more focused tests and added another test for the case when swg messages are larger than 255 bytes.

--- src/anh/network/soe/packet_utilities_unittest.cc
+++ src/anh/network/soe/packet_utilities_unittest.cc
@@ -16,7 +16,9 @@
  You should have received a copy of the GNU General Public License
  along with MMOServer.  If not, see <http://www.gnu.org/licenses/>.
 */
+
 #include <list>
+#include <tuple>
 #include <gtest/gtest.h>
 #include "anh/network/soe/packet_utilities.h"
 #include "anh/byte_buffer.h"
@@ -24,61 +26,148 @@
 
 using namespace anh;
 using namespace network::soe;
-namespace anh {
-namespace network {
-namespace soe {
+using namespace std;
 
-class PacketUtilitiesTests : public testing::Test
-{
-public:
-    ByteBuffer expected_buffer, in_buffer, out_buffer;
-    std::list<ByteBuffer> buffer_list;
-    virtual void SetUp()
-    {
-        expected_buffer.clear();
-        in_buffer.clear();
-        out_buffer.clear();
-        buffer_list.clear();
-    }
-    virtual void TearDown() {}
-    void buildOutputBuffer()
-    {
-        expected_buffer.write<uint16_t>(anh::bigToHost<uint16_t>(0x19)); // header
-        expected_buffer.write<uint8_t>(4); // size
-        expected_buffer.write<int32_t>(5); // value
-        expected_buffer.write<uint8_t>(4); // size
-        expected_buffer.write<int32_t>(5); // value
-        expected_buffer.write<uint8_t>(4); // size
-        expected_buffer.write<int32_t>(5); // value
-    }
+namespace {
+
+class PacketUtilitiesTests : public testing::Test {
+protected:
+    void SetUp() {}
+    void TearDown() {}
+
+    // Returns a tuple containing a list of swg message buffers that are all under
+    // 255 bytes long and the expected multi data channel message when packed.
+    tuple<list<ByteBuffer>, ByteBuffer> generateSmallMultiMessageData();
+
+    // Returns a tuple containing a list of swg message buffers that are all over
+    // 255 bytes long and the expected multi data channel message when packed.
+    tuple<list<ByteBuffer>, ByteBuffer> generateLargeMultiMessageData();
+
+    const static string long_string;
 };
 
-TEST_F(PacketUtilitiesTests, packDataChannelFromSingleMessage)
-{
-    in_buffer.write<int>(5);
-    buffer_list.push_back(in_buffer);
-
+/// This test ensures that when packing only a single swg message no modifications are made
+TEST_F(PacketUtilitiesTests, PackingSingleMessageDoesNotModifyIt) {
+    ByteBuffer small_buffer;
+    small_buffer.write<uint32_t>(500);
+    
+    list<ByteBuffer> buffer_list(1, small_buffer); // Create a list of only 1 buffer
+    
+    ByteBuffer out_buffer;
     packDataChannelMessages(buffer_list, out_buffer);
 
-    EXPECT_EQ(5, out_buffer.read<int>());
+    // Expect that the output buffer has the same value in it as the input.
+    EXPECT_EQ(500, out_buffer.read<int>());
 }
-TEST_F(PacketUtilitiesTests, packetDataFromMessages)
-{
-    in_buffer.write<int>(5);
 
-    std::list<ByteBuffer> buffer_list;
-    buffer_list.push_back(in_buffer);
-    buffer_list.push_back(in_buffer);
-    buffer_list.push_back(in_buffer);
+/// This test ensures that when packing multiple swg messages together that they produce a
+/// single buffer with a 0x0019 header.
+TEST_F(PacketUtilitiesTests, PackingMultipleMessagesAddsMultiMessageHeader) {
+    ByteBuffer small_buffer;
+    small_buffer.write<uint32_t>(500);
+    
+    list<ByteBuffer> buffer_list(2, small_buffer); // Create a list with more than 1 buffer
 
-    buildOutputBuffer();
-
+    ByteBuffer out_buffer;
     packDataChannelMessages(buffer_list, out_buffer);
 
-    EXPECT_EQ(expected_buffer.size(), out_buffer.size());
     EXPECT_EQ(anh::bigToHost<uint16_t>(0x19), out_buffer.read<uint16_t>());
 }
 
-}  // namespace soe
-}  // namespace network
-}  // namespace anh
+
+/// This test ensures that when packing multiple swg messages that any of the messages with
+/// a length smaller than 255 has an uint8_t size prefixed to them.
+TEST_F(PacketUtilitiesTests, SmallSwgMessagesHave8ByteSizePrefix) {
+    list<ByteBuffer> buffer_list;
+    ByteBuffer expected_buffer;
+
+    tie(buffer_list, expected_buffer) = generateSmallMultiMessageData();
+
+    ByteBuffer out_buffer;
+    packDataChannelMessages(buffer_list, out_buffer);
+    
+    EXPECT_EQ(expected_buffer.read<uint16_t>(), out_buffer.read<uint16_t>());
+
+    for (int i = 0; i < 3; ++i) {
+        EXPECT_EQ(expected_buffer.read<uint8_t>(), out_buffer.read<uint8_t>()); // size
+        EXPECT_EQ(expected_buffer.read<uint32_t>(), out_buffer.read<uint32_t>()); // data
+    }
+}
+
+/// This test ensures that when packing multiple swg messages that any of the messages exceeding
+/// a 255 length have a uint16_t size prefixed to them.
+TEST_F(PacketUtilitiesTests, LargeSwgMessagesHave16ByteSizePrefix) {
+    list<ByteBuffer> buffer_list;
+    ByteBuffer expected_buffer;
+
+    tie(buffer_list, expected_buffer) = generateSmallMultiMessageData();
+
+    ByteBuffer out_buffer;
+    packDataChannelMessages(buffer_list, out_buffer);
+    
+    EXPECT_EQ(expected_buffer.read<uint16_t>(), out_buffer.read<uint16_t>());
+
+    for (int i = 0; i < 3; ++i) {
+        EXPECT_EQ(expected_buffer.read<uint16_t>(), out_buffer.read<uint16_t>()); // size
+        EXPECT_EQ(expected_buffer.read<string>(), out_buffer.read<string>()); // data
+    }
+}
+
+
+// Implementation of the PacketUtilitiesTests's helper members
+
+const string PacketUtilitiesTests::long_string = // This string is 300 bytes long
+        "2d8h3941y88gpcu633t7a0o9j0p3658fpnxwlmcxgv9buxy8x2"
+        "2d8h3941y88gpcu633t7a0o9j0p3658fpnxwlmcxgv9buxy8x2"
+        "2d8h3941y88gpcu633t7a0o9j0p3658fpnxwlmcxgv9buxy8x2"
+        "2d8h3941y88gpcu633t7a0o9j0p3658fpnxwlmcxgv9buxy8x2"
+        "2d8h3941y88gpcu633t7a0o9j0p3658fpnxwlmcxgv9buxy8x2"
+        "2d8h3941y88gpcu633t7a0o9j0p3658fpnxwlmcxgv9buxy8x2";
+
+tuple<list<ByteBuffer>, ByteBuffer> PacketUtilitiesTests::generateSmallMultiMessageData() {
+    // Create a small buffer with a single data value that is less than 255 bytes long
+    ByteBuffer small_buffer;
+    small_buffer.write<uint32_t>(500); // This int is only 4 bytes long
+    
+    // Create a list of 3 of the previous buffers to simulate 3 swg messages being packed.
+    list<ByteBuffer> buffer_list(3, small_buffer);
+
+    // Create a buffer that holds the expected output from packing the above data.
+    ByteBuffer expected_buffer;
+    
+    expected_buffer.write<uint16_t>(anh::bigToHost<uint16_t>(0x19)); // header
+    expected_buffer.write<uint8_t>(4); // size
+    expected_buffer.write<int32_t>(500); // value
+    expected_buffer.write<uint8_t>(4); // size
+    expected_buffer.write<int32_t>(500); // value
+    expected_buffer.write<uint8_t>(4); // size
+    expected_buffer.write<int32_t>(500); // value
+
+    // Return a tuple of the list of simulated swg messages and the expected buffer.
+    return make_tuple(buffer_list, expected_buffer);
+}
+
+tuple<list<ByteBuffer>, ByteBuffer> PacketUtilitiesTests::generateLargeMultiMessageData() {
+    // Create a large buffer with a length of 304 bytes (300 byte string + 4 byte string length)
+    ByteBuffer large_buffer;
+    large_buffer.write<string>(long_string); // This int is only 4 bytes long
+    
+    // Create a list of 3 of the previous buffers to simulate 3 swg messages being packed.
+    list<ByteBuffer> buffer_list(3, large_buffer);
+
+    // Create a buffer that holds the expected output from packing the above data.
+    ByteBuffer expected_buffer;
+    
+    expected_buffer.write<uint16_t>(anh::bigToHost<uint16_t>(0x19)); // header
+    expected_buffer.write<uint16_t>(anh::hostToLittle<uint16_t>(304)); // size
+    expected_buffer.write<string>(long_string); // value
+    expected_buffer.write<uint16_t>(anh::hostToLittle<uint16_t>(304)); // size
+    expected_buffer.write<string>(long_string); // value
+    expected_buffer.write<uint16_t>(anh::hostToLittle<uint16_t>(304)); // size
+    expected_buffer.write<string>(long_string); // value
+
+    // Return a tuple of the list of simulated swg messages and the expected buffer.
+    return make_tuple(buffer_list, expected_buffer);
+}
+
+}  // namespace
