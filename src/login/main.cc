08add12f660d92b9df34afe0f36db4d63a46a6b8
Eric Barr <eb@anhstudios.com>
2011-06-22 11:41:52 -0400

    Made login a module

--- src/login/main.cc
+++ src/login/main.cc
@@ -1,66 +1,21 @@
-/*
----------------------------------------------------------------------------------------
-This source file is part of SWG:ANH (Star Wars Galaxies - A New Hope - Server Emulator)
+#include "main.h"
 
-For more information, visit http://www.swganh.com
+bool API Load(std::shared_ptr<anh::module_manager::PlatformServices> services) {
+	return true;
+}
 
-Copyright (c) 2006 - 2011 The SWG:ANH Team */
+bool API Unload(std::shared_ptr<anh::module_manager::PlatformServices> services) {
+	return true;
+}
 
-#include <login/login_app.h>
-#include <anh/event_dispatcher/event_dispatcher.h>
-#include <anh/scripting/scripting_manager.h>
-#include <anh/module_manager/platform_services.h>
-#include <anh/module_manager/module_manager.h>
-#include <anh/clock.h>
+const std::string API GetModuleName(void) {
+	return "DLLTest";
+}
 
-#include <iostream>
-#include <glog/logging.h>
-#include <boost/thread/thread.hpp>
+const anh::module_manager::ModuleApiVersion API GetModuleVersion(void) {
+	return anh::module_manager::ModuleApiVersion(0, 1, "0.1");
+}
 
-using namespace std;
-using namespace anh;
-using namespace scripting;
-using namespace event_dispatcher;
-using namespace module_manager;
-using namespace login;
-int main(int argc, char* argv[])
-{
-    /// pre startup
-    // Initialize the google logging.
-    google::InitGoogleLogging(argv[0]);
-
-    #ifndef _WIN32
-        google::InstallFailureSignalHandler();
-    #endif
-
-    FLAGS_log_dir = "./logs";
-    FLAGS_stderrthreshold = 1;
-    setvbuf( stdout, NULL, _IONBF, 0);
-    // config files
-    list<string> config;
-    config.push_back("config/login.cfg");
-    shared_ptr<EventDispatcherInterface> dispatcher = make_shared<EventDispatcher>();
-    shared_ptr<Clock> clock = make_shared<Clock>();
-    auto services = make_shared<PlatformServices>();
-    // add services
-    services->addService("EventDispatcher", dispatcher);
-    services->addService("Clock", clock);
-    LoginApp app(argc, argv, config, services);
-    ///
-    app.startup();
-    boost::this_thread::sleep(boost::posix_time::milliseconds(5));
-    if (true) {
-        while(true){
-            app.process();
-            if (app.kbHit())
-            {
-                if(cin.get() == 'q')
-                {
-                    app.shutdown();
-                    return 0;
-                }
-            }
-            boost::this_thread::sleep(boost::posix_time::milliseconds(1));
-        }
-    }
+const std::string API GetModuleDescription(void) {
+	return "A test module.";
 }
---
+++ src/login/main.h
@@ -0,0 +1,25 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#include <string>
+
+#include "anh/module_manager/module_interface.h"
+#include "anh/module_manager/platform_services.h"
+#include "anh/module_manager/module_main.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+bool API Load(std::shared_ptr<anh::module_manager::PlatformServices>);
+bool API Unload(std::shared_ptr<anh::module_manager::PlatformServices>);
+const std::string API GetModuleName(void);
+const anh::module_manager::ModuleApiVersion API GetModuleVersion(void);
+const std::string API GetModuleDescription(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __MAIN_H__
