59aad7f7ec90770f3570380b16f3fca7b74b29f2
Eric Barr <eb@anhstudios.com>
2011-07-26 03:49:51 -0400

    Removed old active object implementation.

--- src/anh/utils/active_object.h
+++ src/anh/utils/active_object.h
@@ -15,17 +15,38 @@
 namespace anh {
 namespace utils {
 
+/** 
+ * There are many times when it makes sense to break an object off and run it
+ * concurrently while the rest of the application runs. The ActiveObject is a
+ * reusable facility that encourages the encapsulation of data by using asynchronus
+ * messages to process requests in a private thread. 
+ * 
+ * This implementation is based on boost::asio and uses an io_service coupled with a
+ * strand to facilitate the sequential requirements of an active objects message handling.
+ */
 class ActiveObject {
 public:
     explicit ActiveObject(boost::asio::io_service& io_service)
         : io_service_(io_service)
         , strand_(io_service) {}
 
+    /**
+     * Sends a message to be handled by the ActiveObject's asio strand.
+     *
+     * @param message The message to be handled by the active object.
+     */
     template<typename Handler>
     void Send(Handler message) {
         strand_.post(message);
     }
-
+    
+    /**
+     * Sends a message to be handled by the ActiveObject's asio strand. Delays
+     * execution of the message by a specified period of time.
+     *
+     * @param period The period of time to delay execution of the given message.
+     * @param message The message to be handled by the active object.
+     */
     template<typename Handler>
     void SendDelayed(boost::posix_time::time_duration period, Handler message) {
         auto timer = std::make_shared<boost::asio::deadline_timer>(io_service_);
@@ -33,8 +54,14 @@ public:
         timer_->expires_from_now(period);
         timer_->async_wait(strand_.wrap(message));
     }
-
     
+    /**
+     * Repeatedly sends a message to be handled by the ActiveObject's asio 
+     * strand. The specified period of time determines the interval between resends.
+     *
+     * @param period The period of time to delay execution of the given message.
+     * @param message The message to be handled by the active object.
+     */
     template<typename Handler>
     void SendRepeated(boost::posix_time::time_duration period, Handler message) {
         auto timer = std::make_shared<boost::asio::deadline_timer>(io_service_);
