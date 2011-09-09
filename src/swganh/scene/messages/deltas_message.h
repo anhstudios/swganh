33268fb419c90efc59c20a0a4f616daf39efa27d
Eric Barr <eb@anhstudios.com>
2011-09-09 01:25:36 -0400

    Added baselines and deltas message

---
+++ src/swganh/scene/messages/deltas_message.h
@@ -0,0 +1,24 @@
+
+#ifndef SWGANH_SCENE_MESSAGES_DELTAS_MESSAGE_H_
+#define SWGANH_SCENE_MESSAGES_DELTAS_MESSAGE_H_
+
+#include <cstdint>
+
+#include "anh/byte_buffer.h"
+
+#include "swganh/base/swg_message.h"
+#include "swganh/scene/messages/base_baselines_message.h"
+
+namespace swganh {
+namespace scene {
+namespace messages {
+    
+class DeltasMessage : public BaseBaselineMessage, public swganh::base::SwgMessage<DeltasMessage> {
+public:
+    static const uint16_t opcount = 5;
+    static const uint32_t opcode = 0x12862153;
+};
+    
+}}}  // namespace swganh::scene::messages
+
+#endif  // SWGANH_SCENE_MESSAGES_DELTAS_MESSAGE_H_
