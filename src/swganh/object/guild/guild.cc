0049d4a5b51990ba1aecd9731590994b636ebe7a
Eric Barr <eb@anhstudios.com>
2011-09-20 17:21:20 -0400

    Added boilerplate for guild factory.

---
+++ src/swganh/object/guild/guild.cc
@@ -0,0 +1,9 @@
+
+#include "swganh/object/guild/guild.h"
+
+using namespace swganh::object::guild;
+
+uint32_t Guild::GetType() 
+{ 
+    return Guild::type; 
+}
---
+++ src/swganh/object/guild/guild.h
@@ -0,0 +1,21 @@
+
+#ifndef SWGANH_OBJECT_GUILD_GUILD_H_
+#define SWGANH_OBJECT_GUILD_GUILD_H_
+
+#include "swganh/object/object.h"
+
+namespace swganh {
+namespace object {
+namespace guild {
+
+class Guild : public swganh::object::Object 
+{
+public:
+    // GILD
+    uint32_t GetType();
+    const static uint32_t type = 0x47494C44;
+};
+
+}}} // swganh::object::guild
+
+#endif //SWGANH_OBJECT_GUILD_GUILD_H_
