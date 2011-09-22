a06399a69c54d93da08a971f2722c644d93a5637
Dallin Wellington <dallin.wellington@gmail.com>
2011-09-21 22:55:40 -0700

    Started Creature object.

--- src/swganh/object/creature.h
+++ src/swganh/object/creature.h
@@ -23,15 +23,112 @@
 
 #include "base_tangible.h"
 
+#include <list>
+
 namespace swganh {
 namespace object {
+
+enum POSTURE
+{
+    STANDING = 0,
+    SNEAKING,
+    WALKING,
+    RUNNING,
+    KNEELING,
+    CROUCH_SNEAKING,
+    CROUCH_WALKING,
+    PRONE,
+    CRAWLING,
+    CLIMBING_STATIONARY,
+    CLIMBING,
+    HOVERING,
+    FLING,
+    LYING_DOWN,
+    SITTING,
+    SKILL_ANIMATING,
+    DRIVING_VEHICLE,
+    RIDING_CREATURE,
+    KNOCKED_DOWN,
+    INCAPACITATED,
+    DEAD,
+    BLOCKING
+};
+
+enum STATE
+{
+    COVER = 0,
+    COMBAT,
+    PEACE,
+    AIMING,
+    ALERT,
+    BERSERK,
+    FEIGN_DEATH,
+    COMBAT_ATTITUDE_EVASIVE,
+    COMBAT_ATTITUDE_NORMAL,
+    COMBAT_ATTITUDE_AGGRESSIVE,
+    TUMBLING,
+    RALLIED,
+    STUNNED,
+    BLINDED,
+    DIZZY,
+    INTIMIDATED,
+    IMMOBILIZED,
+    FROZEN,
+    SWIMMING,
+    SITTING_ON_CHAIR,
+    CRAFTING,
+    GLOWING_JEDI,
+    MASK_SCENT,
+    POISONED,
+    BLEEDING,
+    DISEASED,
+    ON_FIRE,
+    RIDING_MOUNT,
+    MOUNTED_CREATURE,
+    PILOTING_SHIP,
+    SHIP_OPERATIONS,
+    SHIP_GUNNER,
+    SHIP_INTERIOR,
+    PILOTING_POB_SHIP
+};
 
 class Creature : public BaseTangible
 {
 public:
-    Creature();
+    Creature(uint32_t bank_credits, uint32_t cash_credits, std::list<std::string> skill_list);
     ~Creature();
 
+    /**
+     *
+     */
+    void SetBankCredits(uint32_t bank_credits);
+
+    /**
+     *
+     */
+    void SetCashCredits(uint32_t cash_credits);
+
+    /**
+     *
+     */
+    void AddSkill(std::string skill);
+
+    /**
+     *
+     */
+    void RemoveSkill(std::string skill);
+
+private:
+    uint32_t    bank_credits_;              // update 1 variable 0
+    uint32_t    cash_credits_;              // update 1 variable 1
+    uint32_t    skill_list_counter_;        // update 1 variable 3
+    std::list<std::string> skill_list_;     // update 1 variable 3
+    uint32_t    posture_;                   // update 3 variable 11
+    uint8_t     faction_rank_;              // update 3 variable 12
+    uint64_t    owner_id_;                  // update 3 variable 13
+    float       scale_;                     // update 3 variable 14
+    uint32_t    battle_fatigue_;            // update 3 variable 15
+    uint64_t    state_bitmask_;             // update 3 variable 16
 
 };
 
