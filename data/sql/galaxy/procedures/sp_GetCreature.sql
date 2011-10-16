7052edc44de5f04bc7147ff5bd0b79d84e986b96
Kyle Craviotto <kronos@swganh.org>
2011-10-15 17:40:59 -0700

    Updated procedures

---
+++ data/sql/galaxy/procedures/sp_GetCreature.sql
@@ -0,0 +1,24 @@
+# --------------------------------------------------------
+# Host:                         127.0.0.1
+# Server version:               5.3.1-MariaDB
+# Server OS:                    Win32
+# HeidiSQL version:             6.0.0.3603
+# Date/time:                    2011-10-15 14:37:16
+# --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+# Dumping structure for procedure galaxy.sp_GetCreature
+DELIMITER //
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_GetCreature`(IN `object_id` BIGINT)
+BEGIN
+call sp_GetObject(object_id);
+select * from creature where id = object_id;
+END//
+DELIMITER ;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
