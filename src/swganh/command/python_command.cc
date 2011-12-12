082653122ba80fc674541674ce207029bb58a1cd
Eric Barr <eb@anhstudios.com>
2011-12-12 12:40:57 -0500

    Moved PythonCommand to the other command related code.

--- src/swganh/command/python_command.cc
+++ src/swganh/command/python_command.cc
@@ -3,10 +3,11 @@
 
 using namespace boost::python;
 using namespace std;
+using namespace swganh::command;
 using namespace swganh::scripting;
 
-PythonCommand::PythonCommand(const std::string& filename)
-: script_(PythonScript(filename))
+PythonCommand::PythonCommand(const CommandProperties& properties)
+: script_(PythonScript(properties.script_hook))
 {}
 
 void PythonCommand::operator()(uint32_t object_id, uint32_t target_id, std::wstring command_string)
