3a2fc1f3a798163e17e82d62d19d07650aae28b0
Eric Barr <eb@anhstudios.com>
2012-01-28 22:07:42 -0500

    Updated script for testing

--- data/scripts/commands/spatialchatinternal.py
+++ data/scripts/commands/spatialchatinternal.py
@@ -1,18 +1,3 @@
 
-import anh
-import anh.messages.object_controller.send_spatial_chat as _send_spatial_chat
-import re
-
-class SpatialChatInternalCommand(anh.base.Command):
-    label = "spatialchatinternal"
-    regex = r'(\d+) (\d+) (\d+) (\d+) (\d+) (.*)'
-    
-    def execute(self, context):
-        match = re.search(regex, context.command_string)
-    
-        if not match: raise RuntimeError("Invalid spatial chat message")
-    
-        _send_spatial_chat(
-                context.object, context.target, 
-                match.group(6), match.group(2), 
-                match.group(3))    
+print('object id is: ')
+print(actor.id)
