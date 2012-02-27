a24e81b2182b7561f7f3dbf2a7b3fd982aa3c78a
Kyle Craviotto <kyle@anhstudios.com>
2012-02-26 21:15:39 -0800

    template for extracting data from python to clean up some code.

--- src/swganh/combat/combat_data.h
+++ src/swganh/combat/combat_data.h
@@ -1,6 +1,7 @@
 #include <cstdint>
 #include <unordered_map>
 
+#include <anh/hash_string.h>
 #include "swganh/command/command_properties.h"
 
 #include <boost/python.hpp>
@@ -23,16 +24,6 @@ struct CombatData : swganh::command::CommandProperties
         GetPythonData(p_object);
     }
 
-    /*template <typename T, typename U>
-    void ExtractData<T>(boost::python::object& p_object, char* key, U& extract_value)
-    {
-        if (p_object.contains(key))
-        {
-            boost::python::extract<T> tmp_x(p_object[key]);
-            if (tmp_x.check())
-                U = tmp_x();
-        }
-    }*/
     float damage_multiplier;
     int accuracy_bonus;
     float speed_multiplier;
@@ -52,12 +43,20 @@ struct CombatData : swganh::command::CommandProperties
     int cone_angle;
     int area_range;
 
-    uint32_t animation_crc;
+    anh::HashString animation_crc;
 
     void GetPythonData(boost::python::object global);
 
-    //std::unordered_map<uint64_t, StateEffect> state_effects;
-    //std::unordered_map<uint64_t, DotEffect> dot_effects;
+    template <typename T>
+    void ExtractData(boost::python::object& p_object, std::string key, T& extract_value)
+    {
+        if (p_object.contains(key))
+        {
+            boost::python::extract<T> tmp_x(p_object[key]);
+            if (tmp_x.check())
+                extract_value = tmp_x();
+        }
+    }
     
     static std::string HIT_spam() { return "_hit"; }
     static std::string BLOCK_spam() { return "_block"; }
