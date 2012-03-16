c4a155737015f66e78cbbfadbdb804de49e90579
Kyle Craviotto <kyle@anhstudios.com>
2012-03-15 19:10:37 -0700

    Fixed xcopy command and added the working_directory to make sure the python command is run in the intended directory.

--- tools/cmake/Win32PostBuild.cmake
+++ tools/cmake/Win32PostBuild.cmake
@@ -77,12 +77,13 @@ add_custom_command(
 add_custom_command(
     TARGET DEPS
     PRE_BUILD
-    COMMAND xcopy "${WIN_PROJECT_SOURCE_DIR}\\tools\\init_modules.py" "${WIN_PROJECT_BINARY_DIR}\\bin\\$\(Configuration\)\\bindings" /Y /C
+    COMMAND xcopy "${WIN_PROJECT_SOURCE_DIR}\\tools\\bindings\\*.py" "${WIN_PROJECT_BINARY_DIR}\\bin\\$\(Configuration\)\\bindings" /Y /I /C
     VERBATIM
 )    
 add_custom_command(
     TARGET DEPS
     PRE_BUILD
     COMMAND python "${WIN_PROJECT_BINARY_DIR}\\bin\\$\(Configuration\)\\bindings\\init_modules.py" 
+	WORKING_DIRECTORY "${WIN_PROJECT_BINARY_DIR}\\bin\\$\(Configuration\)\\bindings\\"
     VERBATIM
 )   
