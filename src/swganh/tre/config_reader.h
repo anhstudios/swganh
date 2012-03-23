268009e9120638f11ab77fccff72427524e344d6
Eric Barr <eb@anhstudios.com>
2012-03-23 10:56:08 -0400

    Rename LiveReader to ConfigReader

--- src/swganh/tre/config_reader.h
+++ src/swganh/tre/config_reader.h
@@ -1,2 +1,9 @@
 // This file is part of SWGANH which is released under GPL v2.
 // See file LICENSE or go to http://swganh.com/LICENSE
+
+#ifndef SWGANH_TRE_LIVE_READER_H_
+#define SWGANH_TRE_LIVE_READER_H_
+
+
+
+#endif  // SWGANH_TRE_LIVE_READER_H_
