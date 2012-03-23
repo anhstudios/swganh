c3936768b42d14763818d6f44076139f9f844a82
John Shandy <johnshandy01@gmail.com>
2012-03-22 20:53:45 -0500

    Added BidAuctionResponseMessage.

---
+++ src/swganh/messages/bid_auction_response_message.h
@@ -0,0 +1,51 @@
+/*
+ This file is part of SWGANH. For more information, visit http://swganh.com
+ 
+ Copyright (c) 2006 - 2011 The SWG:ANH Team
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#ifndef SWGANH_MESSAGES_BID_AUCTION_RESPONSE_MESSAGE_H_
+#define SWGANH_MESSAGES_BID_AUCTION_RESPONSE_MESSAGE_H_
+
+#include <cstdint>
+#include "anh/byte_buffer.h"
+#include "swganh/messages/base_swg_message.h"
+
+namespace swganh {
+namespace messages {
+
+struct BidAuctionResponseMessage : public swganh::messages::BaseSwgMessage<BidAuctionResponseMessage> {
+	static uint16_t opcount() { return 3; }
+	static uint32_t opcode() { return 0xC58A446E; }
+
+	uint64_t item_id;
+	uint32_t status_flag; // 0 = Succeeded, 1 = Auctioneer is Invalid, 2 = Invalid Item, 9 = Not Enough Credits
+
+	void onSerialize(anh::ByteBuffer& buffer) const {
+		buffer.write(item_id);
+		buffer.write(status_flag);
+	}
+
+	void onDeserialize(anh::ByteBuffer buffer) {
+		item_id = buffer.read<uint64_t>();
+		status_flag = buffer.read<uint32_t>();
+	}
+};
+
+}} // namespace swganh::messages
+
+#endif // SWGANH_MESSAGES_BID_AUCTION_RESPONSE_MESSAGE_H_
