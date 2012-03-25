3080a1dc4bb0a841962d67edc0a1ae78a339cfde
unknown <Dallin@Dallin-MacPC.(none)>
2012-03-25 00:46:51 -0700

    Profane, reserved, and developer name fixtures.

--- data/sql/galaxy/scripts/name_reserved.sql
+++ data/sql/galaxy/scripts/name_reserved.sql
@@ -13,6 +13,16 @@ CREATE TABLE IF NOT EXISTS `name_reserved` (
 DELETE FROM `name_reserved`;
     
 /*!40000 ALTER TABLE `name_reserved` DISABLE KEYS */;
+INSERT INTO `name_reserved` (`id`,`name`) VALUES 
+ (1,'Luke'),
+ (2,'Darth'),
+ (3,'Vader'),
+ (4,'Leia'),
+ (5,'Chewbacca'),
+ (6,'Yoda'),
+ (7,'Emperor'),
+ (8,'stormtrooper'),
+ (9,'TK');
 /*!40000 ALTER TABLE `name_reserved` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
