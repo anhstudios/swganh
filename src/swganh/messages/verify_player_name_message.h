95e24528549141dfe85ecb4a0db6d6f9fefa14f4
John Shandy <johnshandy01@gmail.com>
2012-03-25 00:34:00 -0500

    Added VerifyPlayerNameMessage. Unincluded <string> from StomachRequestMessage.

--- src/swganh/messages/verify_player_name_message.h
+++ src/swganh/messages/verify_player_name_message.h
@@ -18,27 +18,38 @@
  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-#ifndef SWGANH_MESSAGES_STOMACH_REQUEST_MESSAGE_H_
-#define SWGANH_MESSAGES_STOMACH_REQUEST_MESSAGE_H_
+#ifndef SWGANH_MESSAGES_VERIFY_PLAYER_NAME_MESSAGE_H_
+#define SWGANH_MESSAGES_VERIFY_PLAYER_NAME_MESSAGE_H_
 
 #include <cstdint>
+#include <string>
 #include "anh/byte_buffer.h"
 #include "swganh/messages/base_swg_message.h"
 
 namespace swganh {
 namespace messages {
 
-struct StomachRequestMessage : public swganh::messages::BaseSwgMessage<StomachRequestMessage> {
-	static uint16_t opcount() { return 1; }
-	static uint32_t opcode() { return 0xB75DD5D7; }
+struct VerifyPlayerNameMessage : public swganh::messages::BaseSwgMessage<VerifyPlayerNameMessage> {
+	static uint16_t opcount() { return 3; }
+	static uint32_t opcode() { return 0xBB8CAD45; }
+	
+	std::wstring name;
+	uint32_t attempts_counter;
+	uint32_t unknown;
 	
 	void onSerialize(anh::ByteBuffer& buffer) const {
+		buffer.write(name);
+		buffer.write(attempts_counter);
+		buffer.write(unknown);
 	}
 
 	void onDeserialize(anh::ByteBuffer buffer) {
+		name = buffer.read<std::wstring>();
+		attempts_counter = buffer.read<uint32_t>();
+		unknown = buffer.read<uint32_t>();
 	}
 };
 
 }} // namespace swganh::messages
 
-#endif // SWGANH_MESSAGES_STOMACH_REQUEST_MESSAGE_H_
+#endif // SWGANH_MESSAGES_VERIFY_PLAYER_NAME_MESSAGE_H_
