3bb35de2331d44f9efdf926bdaf68c04e2336486
Kyle Craviotto <kyle@anhstudios.com>
2012-04-02 20:39:39 -0700

    Updated fictionally reserved names to be lowercase as that's how we are checking them. Also slightly fixed the ui message we are sending back.

--- data/sql/galaxy/scripts/name_fictionally_reserved.sql
+++ data/sql/galaxy/scripts/name_fictionally_reserved.sql
@@ -4,27 +4,27 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
+# Dumping structure for table galaxy.name_fictionally_reserved
+DROP TABLE IF EXISTS `name_fictionally_reserved`;
 CREATE TABLE IF NOT EXISTS `name_fictionally_reserved` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 
-DELETE FROM `name_fictionally_reserved`;
-    
+# Dumping data for table galaxy.name_fictionally_reserved: ~9 rows (approximately)
 /*!40000 ALTER TABLE `name_fictionally_reserved` DISABLE KEYS */;
-INSERT INTO `name_fictionally_reserved` (`id`,`name`) VALUES 
- (1,'Luke'),
- (2,'Darth'),
- (3,'Vader'),
- (4,'Leia'),
- (5,'Chewbacca'),
- (6,'Yoda'),
- (7,'Emperor'),
- (8,'stormtrooper'),
- (9,'TK');
+INSERT INTO `name_fictionally_reserved` (`id`, `name`) VALUES
+	(1, 'luke'),
+	(2, 'darth'),
+	(3, 'vader'),
+	(4, 'leia'),
+	(5, 'chewbacca'),
+	(6, 'yoda'),
+	(7, 'emperor'),
+	(8, 'stormtrooper'),
+	(9, 'tk');
 /*!40000 ALTER TABLE `name_fictionally_reserved` ENABLE KEYS */;
-
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
