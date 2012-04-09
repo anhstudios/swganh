d0c61f5501639f22d8f50c2a637bb584da824bb2
Eric Barr <apathy@ubuntu.(none)>
2012-04-09 16:26:58 -0700

    Added missing std::

--- src/anh/active_object.h
+++ src/anh/active_object.h
@@ -80,7 +80,7 @@ public:
      */
     template<typename Handler>
     void AsyncRepeated(boost::posix_time::time_duration period, Handler&& func) {
-        auto shared_func = std::make_shared<Handler>(move(func));
+        auto shared_func = std::make_shared<Handler>(std::move(func));
 
         Async([this, period, shared_func] () {
             (*shared_func)();
