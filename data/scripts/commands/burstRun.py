7fc97e2d7f3468d0375b952c9fd8031b08c9f1df
Eric Barr <eb@anhstudios.com>
2012-04-16 17:27:15 -0400

    Style fix: Put all function defs at the top of scripts, procedural execution code at the end (follows common shell scripting best practice)

--- data/scripts/commands/burstRun.py
+++ data/scripts/commands/burstRun.py
@@ -1,15 +1,15 @@
 import swgpy.object
 from swgpy.app import *
 
+def EndBurstRun():
+    actor.Controller().SendSystemMessage(swgpy.OutOfBand('cbt_spam', 'burstrun_stop_single', swgpy.ProseType.TT, actor.id), False, False)
+    actor.run_speed = 5.75
+
 if actor:
     # Check for burstrun flag
     if actor.run_speed == 5.75:
         actor.Controller().SendSystemMessage(swgpy.OutOfBand('cbt_spam', 'burstrun_start_single', swgpy.ProseType.TT, actor.id), False, False)
         actor.run_speed = 10.0
-
-def EndBurstRun():
-    actor.Controller().SendSystemMessage(swgpy.OutOfBand('cbt_spam', 'burstrun_stop_single', swgpy.ProseType.TT, actor.id), False, False)
-    actor.run_speed = 5.75
 
 # function to call, duration from now to call it
 event = PythonEvent(EndBurstRun, 15.0)
