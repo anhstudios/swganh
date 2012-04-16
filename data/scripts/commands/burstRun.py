d0ccb1ccd53d4d14965737f4c41895d0cf620faf
Eric Barr <eb@anhstudios.com>
2012-04-16 17:16:29 -0400

    Can pass events to the python wrapped event dispatcher.
    Refactored the PythonEvent and burstRun script to rely on this method of event dispatching.

--- data/scripts/commands/burstRun.py
+++ data/scripts/commands/burstRun.py
@@ -2,14 +2,15 @@ import swgpy.object
 from swgpy.app import *
 
 if actor:
-	# Check for burstrun flag
+    # Check for burstrun flag
     if actor.run_speed == 5.75:
         actor.Controller().SendSystemMessage(swgpy.OutOfBand('cbt_spam', 'burstrun_start_single', swgpy.ProseType.TT, actor.id), False, False)
         actor.run_speed = 10.0
-		
+
 def EndBurstRun():
-	actor.Controller().SendSystemMessage(swgpy.OutOfBand('cbt_spam', 'burstrun_stop_single', swgpy.ProseType.TT, actor.id), False, False)
-	actor.run_speed = 5.75
-	
-# event dispatcher, function to call, duration from now to call it
-PythonEvent(kernel.event_dispatcher(), EndBurstRun, 15.0).trigger()
+    actor.Controller().SendSystemMessage(swgpy.OutOfBand('cbt_spam', 'burstrun_stop_single', swgpy.ProseType.TT, actor.id), False, False)
+    actor.run_speed = 5.75
+
+# function to call, duration from now to call it
+event = PythonEvent(EndBurstRun, 15.0)
+kernel.event_dispatcher().dispatch(event)
