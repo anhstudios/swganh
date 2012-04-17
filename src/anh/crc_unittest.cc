081cc5339ae676054f6c909b90f7732bb372651e
Kyle Craviotto <kyle@anhstudios.com>
2012-04-16 19:16:58 -0700

    switched from GTEST to Boost Test for better compatibility.

--- src/anh/crc_unittest.cc
+++ src/anh/crc_unittest.cc
@@ -17,22 +17,25 @@
  along with MMOServer.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include <gtest/gtest.h>
+#include <boost/test/unit_test.hpp>
 
 #include "anh/crc.h"
 
 using anh::memcrc;
-
+#define BOOST_TEST_MAIN
+#define BOOST_TEST_MODULE ANH CRC
+BOOST_AUTO_TEST_SUITE(ANHCRC)
 /// This test shows how to find the 32bit checksum of a c-style string.
-TEST(CrcTest, CanCrcCstyleStrings) {
-    EXPECT_EQ(0x338BCFAC, memcrc("test"));
-    EXPECT_EQ(0x2643D57C, memcrc("anothertest"));
-    EXPECT_EQ(0x19522193, memcrc("aThirdTest"));
+BOOST_AUTO_TEST_CASE(CanCrcCstyleStrings) {
+    BOOST_CHECK_EQUAL(0x338BCFAC, memcrc("test"));
+    BOOST_CHECK_EQUAL(0x2643D57C, memcrc("anothertest"));
+    BOOST_CHECK_EQUAL(0x19522193, memcrc("aThirdTest"));
 }
 
 /// This test shows how to find the 32bit checksum of a std::string.
-TEST(CrcTest, CanCrcStdStrings) {
-    EXPECT_EQ(0x338BCFAC, memcrc(std::string("test")));
-    EXPECT_EQ(0x2643D57C, memcrc(std::string("anothertest")));
-    EXPECT_EQ(0x19522193, memcrc(std::string("aThirdTest")));
+BOOST_AUTO_TEST_CASE(CanCrcStdStrings) {
+    BOOST_CHECK_EQUAL(0x338BCFAC, memcrc(std::string("test")));
+    BOOST_CHECK_EQUAL(0x2643D57C, memcrc(std::string("anothertest")));
+    BOOST_CHECK_EQUAL(0x19522193, memcrc(std::string("aThirdTest")));
 }
+BOOST_AUTO_TEST_SUITE_END()
