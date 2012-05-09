1bb67682365b0758db1b21c20cf210c200aabe4d
Eric Barr <eb@anhstudios.com>
2012-05-09 19:13:21 -0400

    Expose the remaining member fucntions of the BaseSwgCommand object

--- src/swganh/command/command_binding.cc
+++ src/swganh/command/command_binding.cc
@@ -6,6 +6,9 @@
 #include "anh/python_shared_ptr.h"
 #include <boost/python.hpp>
 
+#include "swganh/app/swganh_kernel.h"
+#include "swganh/object/creature/creature.h"
+#include "swganh/object/tangible/tangible.h"
 #include "swganh/scripting/utilities.h"
 
 #include "base_swg_command.h"
@@ -17,33 +20,6 @@ using swganh::command::CommandInterface;
 using swganh::command::CommandProperties;
 using swganh::object::ObjectController;
 using swganh::scripting::ScopedGilLock;
-
-struct CommandWrapper : CommandInterface, bp::wrapper<CommandInterface>
-{    
-    const std::shared_ptr<swganh::object::ObjectController>& GetController() const
-    {         
-        ScopedGilLock lock;
-        return this->get_override("GetController")().as<const std::shared_ptr<swganh::object::ObjectController>&>();
-    }
-
-    void Setup()
-    {
-        ScopedGilLock lock;
-        this->get_override("Setup")();
-    }
-
-    bool Validate()
-    {
-        ScopedGilLock lock;
-        return this->get_override("Validate")();
-    }
-
-    void Run()
-    {
-        ScopedGilLock lock;
-        this->get_override("Run")();
-    }
-};
 
 struct BaseSwgCommandWrapper : BaseSwgCommand, bp::wrapper<BaseSwgCommand>
 {
@@ -60,19 +36,21 @@ struct BaseSwgCommandWrapper : BaseSwgCommand, bp::wrapper<BaseSwgCommand>
     }
 
     const std::shared_ptr<ObjectController>& GetController() const
-    {        
-        ScopedGilLock lock;
-        try 
+    {
         {
-            bp::override get_controller = this->get_override("GetController");
-            if (get_controller)
+            ScopedGilLock lock;
+            try 
             {
-                return get_controller().as<const std::shared_ptr<ObjectController>&>();
+                bp::override get_controller = this->get_override("GetController");
+                if (get_controller)
+                {
+                    return get_controller().as<const std::shared_ptr<ObjectController>&>();
+                }
             }
-        }
-        catch(bp::error_already_set& /*e*/)
-        {
-            PyErr_Print();
+            catch(bp::error_already_set& /*e*/)
+            {
+                PyErr_Print();
+            }
         }
 
         return this->BaseSwgCommand::GetController();
@@ -81,33 +59,70 @@ struct BaseSwgCommandWrapper : BaseSwgCommand, bp::wrapper<BaseSwgCommand>
     void Setup()
     {
         ScopedGilLock lock;
-        this->get_override("Setup")();
+        try 
+        {
+            this->get_override("Setup")();
+        }
+        catch(bp::error_already_set& /*e*/)
+        {
+            PyErr_Print();
+        }
     }
 
     bool Validate()
     {
+        bool validated = false;
+
         ScopedGilLock lock;
-        return this->get_override("Validate")();
+        try 
+        {
+            validated = this->get_override("Validate")();
+        }
+        catch(bp::error_already_set& /*e*/)
+        {
+            PyErr_Print();
+        }
+
+        return validated;
     }
 
     void Run()
     {
         ScopedGilLock lock;
-        this->get_override("Run")();
+        try 
+        {
+            this->get_override("Run")();
+        }
+        catch(bp::error_already_set& /*e*/)
+        {
+            PyErr_Print();
+        }
     }
 };
 
 void swganh::command::ExportCommand()
 {
-    bp::class_<CommandInterface, CommandWrapper, boost::noncopyable>("CommandInterface", bp::no_init)
-        .def("GetController", bp::pure_virtual(&CommandWrapper::GetController), bp::return_internal_reference<>())
-        .def("Setup", bp::pure_virtual(&CommandWrapper::Setup))
-        .def("Validate", bp::pure_virtual(&CommandWrapper::Validate))
-        .def("Run", bp::pure_virtual(&CommandWrapper::Run))
+    bp::class_<CommandInterface, boost::noncopyable>("CommandInterface", bp::no_init)
+        .def("GetController", bp::pure_virtual(&CommandInterface::GetController), bp::return_internal_reference<>())
+        .def("Setup", bp::pure_virtual(&CommandInterface::Setup))
+        .def("Validate", bp::pure_virtual(&CommandInterface::Validate))
+        .def("Run", bp::pure_virtual(&CommandInterface::Run))
     ;
     
     bp::class_<BaseSwgCommand, BaseSwgCommandWrapper, bp::bases<CommandInterface>, boost::noncopyable>
         ("BaseSwgCommand", bp::init<swganh::app::SwganhKernel*, const CommandProperties&, const std::shared_ptr<object::ObjectController>&, const swganh::messages::controllers::CommandQueueEnqueue&>())
-        .def("GetController", &BaseSwgCommandWrapper::GetController, bp::return_internal_reference<>())
+        .def("GetController", &BaseSwgCommandWrapper::GetController, bp::return_value_policy<bp::copy_const_reference>())
+        .def("GetKernel", &BaseSwgCommandWrapper::GetKernel, bp::return_internal_reference<>())
+        .def("GetCommandName", &BaseSwgCommandWrapper::GetCommandName)
+        .def("GetActionCounter", &BaseSwgCommandWrapper::GetActionCounter)
+        .def("GetPriority", &BaseSwgCommandWrapper::GetPriority)
+        .def("GetCommandGroup", &BaseSwgCommandWrapper::GetCommandGroup)
+        .def("GetTargetRequiredType", &BaseSwgCommandWrapper::GetTargetRequiredType)
+        .def("GetMaxRangeToTarget", &BaseSwgCommandWrapper::GetMaxRangeToTarget)
+        .def("GetDefaultTime", &BaseSwgCommandWrapper::GetDefaultTime)
+        .def("GetRequiredAbility", &BaseSwgCommandWrapper::GetRequiredAbility)
+        .def("IsQueuedCommand", &BaseSwgCommandWrapper::IsQueuedCommand)
+        .def("GetActor", &BaseSwgCommandWrapper::GetActor, bp::return_value_policy<bp::copy_const_reference>())
+        .def("GetTarget", &BaseSwgCommandWrapper::GetTarget, bp::return_value_policy<bp::copy_const_reference>())
     ;
 }
