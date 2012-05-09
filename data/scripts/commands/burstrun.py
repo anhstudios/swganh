476f74890abf46d6b62a69fd688184ba265623cb
Eric Barr <eb@anhstudios.com>
2012-05-09 19:13:00 -0400

    Updating burst run to latest format

--- data/scripts/commands/burstrun.py
+++ data/scripts/commands/burstrun.py
@@ -2,13 +2,9 @@ import swgpy
 
 class BurstRunCommand(swgpy.command.BaseSwgCommand):
     #this data exists in all instances of burst run.
-    base_run_multiplier = 10.0
+    base_run_multiplier = 1.7
     base_run_duration = 15.0
     base_cooldate_timer = 60.0
-
-    def __init__(self, kernel, properties, controller, command_request):
-        super(BurstRunCommand, self).__init__(kernel, properties, controller, command_request)
-        print('Intializing BurstRun')
 
     def Validate(self):
         #if actor.has_flag("BurstRunning"):
@@ -17,14 +13,15 @@ class BurstRunCommand(swgpy.command.BaseSwgCommand):
         return True
 
     def Run(self):
-        #if actor && !actor.has_flag("BurstRunning"):
-        #    actor.Controller().SendSystemMessage(swgpy.OutOfBand('cbt_spam', 'burstrun_start_single', swgpy.ProseType.TT, actor.id), False, False)
-        #    actor.run_speed *= base_run_multiplier
+        actor = self.GetActor()
+        if actor:
+            actor.Controller().SendSystemMessage(swgpy.OutOfBand('cbt_spam', 'burstrun_start_single', swgpy.ProseType.TT, actor.id), False, False)
+            actor.run_speed *= self.base_run_multiplier
 
         #    # function to call, duration from now to call it
         #    return PythonCallback(EndBurstRun, self, base_run_duration)
-        print('Executing BurstRunning')
 
     def EndBurstRun(self):
+        actor = self.GetActor()
         actor.Controller().SendSystemMessage(swgpy.OutOfBand('cbt_spam', 'burstrun_stop_single', swgpy.ProseType.TT, actor.id), False, False)
-        actor.run_speed /= base_run_multiplier
+        actor.run_speed /= self.base_run_multiplier
