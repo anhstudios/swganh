daada1e48116be5ca8eac24b3bcd1ed6a50f1e2b
Eric Barr <eb@anhstudios.com>
2012-05-13 22:53:40 -0400

    Fixed python runtime errors

--- data/scripts/commands/duel.py
+++ data/scripts/commands/duel.py
@@ -1,5 +1,5 @@
 from swgpy.command import BaseSwgCommand
-from swgpy import PVPSTATUS
+from swgpy import PVPSTATUS, OutOfBand, ProseType
 
 class DuelCommand(BaseSwgCommand):
     def Run(self):
@@ -11,12 +11,12 @@ class DuelCommand(BaseSwgCommand):
         if not actor.in_duel_list(target.id):
             actor.add_duel_list(target.id)
             #system message
-            Acontroller.SendSystemMessage(swgpy.OutOfBand("duel", "challenge_self", swgpy.ProseType.TT, target.id), False, False)
-            Tcontroller.SendSystemMessage(swgpy.OutOfBand("duel", "challenge_target", swgpy.ProseType.TT, actor.id), False, False)
+            Acontroller.SendSystemMessage(OutOfBand("duel", "challenge_self", ProseType.TT, target.id), False, False)
+            Tcontroller.SendSystemMessage(OutOfBand("duel", "challenge_target", ProseType.TT, actor.id), False, False)
         if actor.in_duel_list(target.id) and target.in_duel_list(actor.id):
             # Start The Duel
-            Acontroller.SendSystemMessage(swgpy.OutOfBand("duel", "accept_target", swgpy.ProseType.TT, target.id), False, False)
-            Tcontroller.SendSystemMessage(swgpy.OutOfBand("duel", "accept_self", swgpy.ProseType.TT, actor.id), False, False)
+            Acontroller.SendSystemMessage(OutOfBand("duel", "accept_target", ProseType.TT, target.id), False, False)
+            Tcontroller.SendSystemMessage(OutOfBand("duel", "accept_self", ProseType.TT, actor.id), False, False)
             actor.pvp_status = PVPSTATUS.PvPStatus_Attackable
             target.pvp_status = PVPSTATUS.PvPStatus_Attackable
             actor.activate_auto_attack();
