d78b3719fae476e5f8fe96b129a4f954cb827ba0
Richard Laughlin <rwl3564@rit.edu>
2012-08-09 22:57:50 -0400

    Completed slot implementations

---
+++ src/swganh/object/slot_interface.h
@@ -0,0 +1,24 @@
+#ifndef SLOT_INTERFACE_H_
+#define SLOT_INTERFACE_H_
+
+#include <memory>
+#include <functional>
+
+namespace swganh {
+namespace object {
+
+	class Object;
+
+	class SlotInterface
+	{
+	public:
+		virtual std::shared_ptr<swganh::object::Object> insert_object(std::shared_ptr<swganh::object::Object> insertObject) = 0;
+		virtual void remove_object(std::shared_ptr<swganh::object::Object> removeObject) = 0;
+
+		virtual void view_objects(std::function<void(std::shared_ptr<swganh::object::Object>&)> walkerFunction) = 0;
+	};
+
+}
+}
+
+#endif
