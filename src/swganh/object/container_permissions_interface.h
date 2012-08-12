61123e46adc605db8500a1a541225baab30e9a66
Richard Laughlin <rwl3564@rit.edu>
2012-08-12 17:57:28 -0400

    Added container permissions

---
+++ src/swganh/object/container_permissions_interface.h
@@ -0,0 +1,45 @@
+#ifndef SWGANH_CONTAINER_PERMISSIONS_INTERFACE_H_
+#define SWGANH_CONTAINER_PERMISSIONS_INTERFACE_H_
+
+namespace swganh
+{
+namespace object
+{
+
+	class Object;
+
+	class ContainerPermissionsInterface
+	{
+	public:
+
+		virtual bool canInsert(std:shared_ptr<Object> container, std::shared_ptr<Object> requester, std::shared_ptr<Object> object) = 0;
+
+		virtual bool canRemove(std::shared_ptr<Object> container, std::shared_ptr<Object> requester, std::shared_ptr<Object> object) = 0;
+
+		virtual bool canView(std::shared_ptr<Object> container, std::shared_ptr<Object> requester) = 0;
+
+	};
+
+	class DefaultContainerPermissions : public ContainerPermissionsInterface
+	{
+	public:
+		bool canInsert(std:shared_ptr<Object> container, std::shared_ptr<Object> requester, std::shared_ptr<Object> object)
+		{
+			return false;
+		}
+
+		bool canRemove(std::shared_ptr<Object> container, std::shared_ptr<Object> requester, std::shared_ptr<Object> object)
+		{
+			return false;
+		}
+
+		bool canView(std::shared_ptr<Object> container, std::shared_ptr<Object> requester)
+		{
+			return false;
+		}
+	};
+
+}
+}
+
+#endif
