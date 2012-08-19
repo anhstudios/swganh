2537c4acefc9fdaf9aa09ef1e6c4a039342d6c57
Kyle Craviotto <im.kyle@gmail.com>
2012-08-18 23:34:02 -0700

    fixed docstring

--- src/swganh/app/swganh_kernel_binding.h
+++ src/swganh/app/swganh_kernel_binding.h
@@ -20,6 +20,7 @@
 #include "swganh/social/social_service_interface.h"
 #include "swganh/simulation/simulation_service_interface.h"
 #include "swganh/sui/sui_service_interface.h"
+#include "swganh/equipment/equipment_service_interface.h"
 
 #include <boost/python.hpp>
 
@@ -43,7 +44,7 @@ void exportSWGANHKernel()
                std::bind(&anh::service::ServiceManager::GetService<swganh::combat::CombatServiceInterface>, std::placeholders::_1, "CombatService"),
                return_value_policy<reference_existing_object>(),
                 boost::mpl::vector<swganh::combat::CombatServiceInterface*, anh::service::ServiceManager*>()),
-                "returns an internal refrence of the :class:`.CombatService`")
+                "returns an internal refrence of the :class:`.CombatServiceInterface`")
        .def("command_service", make_function(
                std::bind(&anh::service::ServiceManager::GetService<swganh::command::CommandServiceInterface>, std::placeholders::_1, "CommandService"),
                return_value_policy<reference_existing_object>(),
@@ -64,6 +65,11 @@ void exportSWGANHKernel()
                 return_value_policy<reference_existing_object>(),
                 boost::mpl::vector<swganh::sui::SUIServiceInterface*, anh::service::ServiceManager*>()),
 				"returns an internal refrence of the :class:`.SUIService`")
+		.def("equipment_service", make_function(
+				std::bind(&anh::service::ServiceManager::GetService<swganh::equipment::EquipmentServiceInterface>, std::placeholders::_1, "EquipmentService"),
+                return_value_policy<reference_existing_object>(),
+                boost::mpl::vector<swganh::equipment::EquipmentServiceInterface*, anh::service::ServiceManager*>()),
+				"returns an internal refrence of the :class:`.EquipmentService`")
        ;
        
 }
