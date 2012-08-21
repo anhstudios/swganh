7e01e319629b31042bb299310dfb9b5c8e544c48
Kyle Craviotto <im.kyle@gmail.com>
2012-08-20 20:45:37 -0700

    Added getTargetCreature, so we can get the target as a creature (if available), this Fixes Issue #406

--- data/scripts/commands/endduel.py
+++ data/scripts/commands/endduel.py
@@ -1,11 +1,12 @@
 from swgpy.command import BaseSwgCommand
-
+from swgpy import combat
 class EndDuelCommand(BaseSwgCommand):
     def Run(self):
         actor = self.GetActor()
-        target = self.GetTarget()
+        target = self.GetTargetCreature()
         
         service_mgr = self.GetKernel().service_manager()
+        
         combat_svc = service_mgr.combat_service()
         if (actor and target):
             combat_svc.end_duel(actor, target)
