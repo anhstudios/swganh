7c32de672a30bdc7ef1589d83d883b442a7ceece
Richard Laughlin <rwl3564@rit.edu>
2012-10-04 09:32:36 -0300

    Update src/swganh/app/swganh_app.cc
    Added ascii art and db error exception rethrow

--- src/swganh/app/swganh_app.cc
+++ src/swganh/app/swganh_app.cc
@@ -164,6 +164,18 @@ void SwganhApp::Initialize(int argc, char* argv[]) {
     // Init Logging
     SetupLogging_();
     
+    std::cout << "Ben Kenobi : That team is our last hope." << std::endl;
+    std::cout << "      Yoda :  NO! There is another..." << std::endl << std::endl;
+    std::cout << " .d8888b.  888       888  .d8888b.         d8888 888b    888 888    888 " << std::endl;
+	std::cout << "d88P  Y88b 888   o   888 d88P  Y88b       d88888 8888b   888 888    888 " << std::endl;
+	std::cout << "Y88b.      888  d8b  888 888    888      d88P888 88888b  888 888    888 " << std::endl;
+	std::cout << " \"Y888b.   888 d888b 888 888            d88P 888 888Y88b 888 8888888888 " << std::endl;
+	std::cout << "    \"Y88b. 888d88888b888 888  88888    d88P  888 888 Y88b888 888    888 " << std::endl;
+	std::cout << "      \"888 88888P Y88888 888    888   d88P   888 888  Y88888 888    888 " << std::endl;
+	std::cout << "Y88b  d88P 8888P   Y8888 Y88b  d88P  d8888888888 888   Y8888 888    888 " << std::endl;
+	std::cout << " \"Y8888P\"  888P     Y888  \"Y8888P88 d88P     888 888    Y888 888    888 " << std::endl << std::endl;
+ 
+    
     // Load the configuration    
     LoadAppConfig_(argc, argv);
 
@@ -187,9 +199,10 @@ void SwganhApp::Initialize(int argc, char* argv[]) {
 	    app_config.galaxy_db.password);
     
     } 
-    catch(...)
+    catch(std::exception& e)
     {
     	LOG(fatal) << "Database connection errors occurred. Did you forget to populate the db?";
+    	throw e;
     }
     
     CleanupServices_();
