e48efbf36757ca5b84149bdd685ece00ddc90238
Richard Laughlin <rwl3564@rit.edu>
2013-01-12 15:44:39 -0500

    Added some caching to improve start up times.

--- data/scripts/load_objects.py
+++ data/scripts/load_objects.py
@@ -14,17 +14,31 @@ def addTemplate(template):
 	else:
 		templates[template.name] = template
 
-subdirs = []
-files = []
-for p in sys.path:
-	for dirname, dirnames, filenames in os.walk(p + '/templates'):
-		for subdirname in dirnames:
-			for f in glob.glob(dirname + os.sep + subdirname+ os.sep + '*.py'):
-				files.append(os.path.relpath(f))
-		for fil in glob.glob(dirname + os.sep + '*.py'):
-			files.append(os.path.relpath(fil))
-for file in files:
-	file = file.replace('..' + os.sep, '').replace('./','').replace(os.sep, '.').replace('.py', '').replace('data.scripts.', '')
-	if not '__init__' in file:
-		module = importlib.import_module(file)
-		module.loadTemplates(addTemplate)
+
+template_files = []
+try:
+	for line in open('object_cache.tmp'):
+		template_files.append(line.strip())
+except IOError as e:
+	#This is the first load, so we can't take advantage of caching.
+	cache_out = open('object_cache.tmp', 'w')
+
+	files = []
+	subdirs = []
+	for p in sys.path:
+		for dirname, dirnames, filenames in os.walk(p + '/templates'):
+			for subdirname in dirnames:
+				for f in glob.glob(dirname + os.sep + subdirname+ os.sep + '*.py'):
+					files.append(os.path.relpath(f))
+			for fil in glob.glob(dirname + os.sep + '*.py'):
+				files.append(os.path.relpath(fil))
+				
+	for file in files:
+		file = file.replace('..' + os.sep, '').replace('./','').replace(os.sep, '.').replace('.py', '').replace('data.scripts.', '')
+		if not '__init__' in file:
+			template_files.append(file)
+			cache_out.write(file+'\n');
+			
+for file in template_files:
+	module = importlib.import_module(file)
+	module.loadTemplates(addTemplate)
