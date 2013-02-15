b48db2a9eb8af45e1f65a2b8f9b1033a388966a7
Eric Barr <eric@apathystudios.com>
2013-02-14 21:46:08 -0700

    Fixed indents that were causing issues during character creation

--- data/scripts/templates/object/tangible/wearables/pants/shared_pants_s29.py
+++ data/scripts/templates/object/tangible/wearables/pants/shared_pants_s29.py
@@ -2,17 +2,17 @@
 #### MODIFICATIONS MAY BE LOST IF DONE IMPROPERLY
 #### PLEASE SEE THE ONLINE DOCUMENTATION FOR EXAMPLES
 
-from swgpy.object import *	
+from swgpy.object import *
 
 def create(kernel):
 	result = Tangible()
 
 	result.template = "object/tangible/wearables/pants/shared_pants_s29.iff"
 	result.attribute_template_id = 11
-	result.stfName("wearables_name","pants_s29")		
-	
+	result.stfName("wearables_name","pants_s29")
+
 	#### BEGIN MODIFICATIONS ####
-		result.max_condition = 1000
+	result.max_condition = 1000
 	####  END MODIFICATIONS  ####
-	
+
 	return result
--- data/scripts/templates/object/tangible/wearables/shirt/shared_shirt_s34.py
+++ data/scripts/templates/object/tangible/wearables/shirt/shared_shirt_s34.py
@@ -2,17 +2,17 @@
 #### MODIFICATIONS MAY BE LOST IF DONE IMPROPERLY
 #### PLEASE SEE THE ONLINE DOCUMENTATION FOR EXAMPLES
 
-from swgpy.object import *	
+from swgpy.object import *
 
 def create(kernel):
 	result = Tangible()
 
 	result.template = "object/tangible/wearables/shirt/shared_shirt_s34.iff"
 	result.attribute_template_id = 11
-	result.stfName("wearables_name","shirt_s34")		
-	
+	result.stfName("wearables_name","shirt_s34")
+
 	#### BEGIN MODIFICATIONS ####
-		result.max_condition = 1000
+	result.max_condition = 1000
 	####  END MODIFICATIONS  ####
-	
+
 	return result
