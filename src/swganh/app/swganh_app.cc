153349ce9dba2c19b35a680bcc82d3af0219ebd4
Eric Barr <eric@apathystudios.com>
2013-02-18 20:05:15 -0700

    Version printout now uses defines instead of hardcoding

--- src/swganh/app/swganh_app.cc
+++ src/swganh/app/swganh_app.cc
@@ -176,7 +176,7 @@ void SwganhApp::Initialize(int argc, char* argv[]) {
 	std::cout << "      \"888 88888P Y88888 888    888   d88P   888 888  Y88888 888    888 " << std::endl;
 	std::cout << "Y88b  d88P 8888P   Y8888 Y88b  d88P  d8888888888 888   Y8888 888    888 " << std::endl;
 	std::cout << " \"Y8888P\"  888P     Y888  \"Y8888P88 d88P     888 888    Y888 888    888 " << std::endl << std::endl;
-	std::cout << "                             Release 0.5.1 "<< std::endl << std::endl << std::endl;
+	std::cout << "                             Release " << VERSION_MAJOR << "." << VERSION_MINOR << "." << VERSION_PATCH << " "<< std::endl << std::endl << std::endl;
  
     
     // Load the configuration    
