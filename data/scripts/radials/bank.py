ea98ae8d3939dcba5dba661c442288850d74878b
Dallin Wellington <dallin.wellington@gmail.com>
2013-02-20 16:39:21 -0700

    Bank radial script fix.
    Updated the Bank radial script.

--- data/scripts/radials/bank.py
+++ data/scripts/radials/bank.py
@@ -32,11 +32,11 @@ class PyRadialMenu(RadialMenu):
 				bank = self.getKernel().serviceManager().equipmentService().getEquippedObject(owner, "bank")
 				player_service.openContainer(owner, bank)
 			elif action == RadialIdentifier.bankJoin:
-				joinBank(self, owner)
+				self.joinBank(owner)
 			elif action == RadialIdentifier.bankQuit:
-				quitBank(self, owner)
+				self.quitBank(owner)
 			elif action == RadialIdentifier.bankTransfer:
-				transferBank(self, owner, target)
+				self.transferBank(owner, target)
 			elif action == RadialIdentifier.bankWithdrawAll:
 				amount = owner.bank_credits
 				owner.cash_credits += owner.bank_credits
@@ -48,63 +48,63 @@ class PyRadialMenu(RadialMenu):
 				owner.cash_credits = 0
 				SystemMessage.sendSystemMessage(owner, swgpy.OutOfBand('base_player', 'prose_deposit_success', swgpy.ProseType.DI, amount), False, False)
 			
-def joinBank(self, owner):
-	bank = self.getKernel().serviceManager().equipmentService().getEquippedObject(owner, "bank")
-	if bank:
-		if bank.scene_id == owner.scene_id:
-			SystemMessage.sendSystemMessage(owner, 'system_msg', 'already_member_of_bank', False, False)
-		elif bank.scene_id == 0:
-			bank.scene_id = owner.scene_id
-			SystemMessage.sendSystemMessage(owner, swgpy.OutOfBand('system_msg', 'successfully_joined_bank'), False, False)
-		else:
-			SystemMessage.sendSystemMessage(owner, swgpy.OutOfBand('system_msg', 'member_of_different_bank'), False, False)
-def quitBank(self, owner):
-	bank = self.getKernel().serviceManager().equipmentService().getEquippedObject(owner, "bank")
-	if bank:
-		if bank.hasContainedObjects():
-			bank.scene_id = 0
-			SystemMessage.sendSystemMessage(owner, swgpy.OutOfBand('system_msg', 'successfully_quit_bank'), False, False)		
-		else:
-			SystemMessage.sendSystemMessage(owner, swgpy.OutOfBand('system_msg', 'bank_not_empty'), False, False)	
-def transferBank(self, owner, target):
-	sui = self.getKernel().serviceManager().suiService()
-	
-	#Enforce uniqueness
-	if sui.getSUIWindowByScriptName(owner, 'Script.transfer') != None:
-		return
-
-	window = sui.createSUIWindow('Script.transfer', owner, target, 10)
-	window.setProperty('transaction.txtInputFrom:Text', 'From')
-	window.setProperty('transaction.txtInputTo:Text', 'To')	
-	window.setProperty('bg.caption.lblTitle:Text', '@base_player:bank_title')
-	window.setProperty('Prompt.lblPrompt:Text', '@base_player:bank_prompt')
-	window.setProperty('transaction.lblFrom:Text', '@ui:inv_money_cash')
-	window.setProperty('transaction.lblTo:Text', '@ui:inv_money_bank')	
-	window.setProperty('transaction.lblStartingFrom:Text', str(owner.cash_credits))
-	window.setProperty('transaction.lblStartingTo:Text', str(owner.bank_credits))
-	window.setProperty('transaction.txtInputFrom:Text', str(owner.cash_credits))
-	window.setProperty('transaction.txtInputTo:Text', str(owner.bank_credits))
-	window.setProperty('transaction:ConversionRatioFrom', '1')
-	window.setProperty('transaction:ConversionRatioTo', '1')
-	results = ResultList()
-	results.extend(
-		['transaction.txtInputFrom:Text','transaction.txtInputTo:Text']
-	)
-	transferCallback = PythonCallback('radials.bank', 'transferCallback')
-	window.subscribeToEventCallback(0, '', InputTrigger.OK, results, transferCallback)
-	window.subscribeToEventCallback(1, '', InputTrigger.CANCEL, results, transferCallback)
-	sui.openSUIWindow(window)
-	
-def transferCallback(owner, event_id, results):
-	if event_id == 0 and len(results) == 2:
-		cash = int(results[0])
-		bank = int(results[1])
-		if cash + bank == owner.cash_credits + owner.bank_credits:
-			owner.bank_credits = bank
-			owner.cash_credits = cash
-			SystemMessage.sendSystemMessage(owner, swgpy.OutOfBand('base_player', 'bank_success'), False, False)
-		else:
-			SystemMessage.sendSystemMessage(owner, 'NOPE', False, False)
-	return True
-			
+	def joinBank(self, owner):
+		bank = self.getKernel().serviceManager().equipmentService().getEquippedObject(owner, "bank")
+		if bank:
+			if bank.scene_id == owner.scene_id:
+				SystemMessage.sendSystemMessage(owner, 'system_msg', 'already_member_of_bank', False, False)
+			elif bank.scene_id == 0:
+				bank.scene_id = owner.scene_id
+				SystemMessage.sendSystemMessage(owner, swgpy.OutOfBand('system_msg', 'succesfully_joined_bank'), False, False)
+			else:
+				SystemMessage.sendSystemMessage(owner, swgpy.OutOfBand('system_msg', 'member_of_different_bank'), False, False)
+	def quitBank(self, owner):
+		bank = self.getKernel().serviceManager().equipmentService().getEquippedObject(owner, "bank")
+		if bank:
+			if bank.hasContainedObjects() is not 0:
+				bank.scene_id = 0
+				SystemMessage.sendSystemMessage(owner, swgpy.OutOfBand('system_msg', 'succesfully_quit_bank'), False, False)		
+			else:
+				SystemMessage.sendSystemMessage(owner, swgpy.OutOfBand('system_msg', 'bank_not_empty'), False, False)	
+	def transferBank(self, owner, target):
+		sui = self.getKernel().serviceManager().suiService()
 		
+		#Enforce uniqueness
+		if sui.getSUIWindowByScriptName(owner, 'Script.transfer') != None:
+			return
+
+		window = sui.createSUIWindow('Script.transfer', owner, target, 10)
+		window.setProperty('transaction.txtInputFrom:Text', 'From')
+		window.setProperty('transaction.txtInputTo:Text', 'To')	
+		window.setProperty('bg.caption.lblTitle:Text', '@base_player:bank_title')
+		window.setProperty('Prompt.lblPrompt:Text', '@base_player:bank_prompt')
+		window.setProperty('transaction.lblFrom:Text', '@ui:inv_money_cash')
+		window.setProperty('transaction.lblTo:Text', '@ui:inv_money_bank')	
+		window.setProperty('transaction.lblStartingFrom:Text', str(owner.cash_credits))
+		window.setProperty('transaction.lblStartingTo:Text', str(owner.bank_credits))
+		window.setProperty('transaction.txtInputFrom:Text', str(owner.cash_credits))
+		window.setProperty('transaction.txtInputTo:Text', str(owner.bank_credits))
+		window.setProperty('transaction:ConversionRatioFrom', '1')
+		window.setProperty('transaction:ConversionRatioTo', '1')
+		results = ResultList()
+		results.extend(
+			['transaction.txtInputFrom:Text','transaction.txtInputTo:Text']
+		)
+		transferCallback = PythonCallback(self, 'transferCallback')
+		window.subscribeToEventCallback(0, '', InputTrigger.OK, results, transferCallback)
+		window.subscribeToEventCallback(1, '', InputTrigger.CANCEL, results, transferCallback)
+		sui.openSUIWindow(window)
+		
+	def transferCallback(self, owner, event_id, results):
+		if event_id == 0 and len(results) == 2:
+			cash = int(results[0])
+			bank = int(results[1])
+			if cash + bank == owner.cash_credits + owner.bank_credits:
+				owner.bank_credits = bank
+				owner.cash_credits = cash
+				SystemMessage.sendSystemMessage(owner, swgpy.OutOfBand('base_player', 'bank_success'), False, False)
+			else:
+				SystemMessage.sendSystemMessage(owner, 'NOPE', False, False)
+		return True
+				
+			
