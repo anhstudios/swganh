8bd3418ee555cf8c6e41730745a81fe72138d56d
rcythr <rwl3564@rit.edu>
2013-03-18 20:19:24 -0600

    Fixed wookies. Stupid hairy animals.

--- data/scripts/character_create/starting_items.py
+++ data/scripts/character_create/starting_items.py
@@ -1,15 +1,11 @@
 
-
-
 def GetStartingItems(species, profession, gender):
 	#Grab items common to all
 	itemList = list(CommonItems)
 	
 	#Grab items based on species and profession
-	if species == 'wookiee':
-		pass
-	elif species == 'ithorian':
-		itemList.extend(IthorianItems[gender + ':' + profession])
+	if species == 'wookiee' or species == 'ithorian':
+		itemList.extend(StartingItems[species+":"+gender + ':' + profession])
 	else:
 		itemList.extend(StartingItems[gender+":"+profession])
 	
@@ -79,109 +75,155 @@ StartingItems = {
 										'object/tangible/wearables/shoes/shared_shoes_s07.iff'
 									),
 	
-	'male:crafting_artisan' : 		(	
-										'object/tangible/crafting/station/shared_generic_tool.iff',
-										'object/tangible/survey_tool/shared_survey_tool_mineral.iff', 
-										'object/tangible/wearables/shirt/shared_shirt_s34.iff',
-										'object/tangible/wearables/pants/shared_pants_s29.iff',
-										'object/tangible/wearables/shoes/shared_shoes_s01.iff'
-									),
+	'male:crafting_artisan' : 			(	
+											'object/tangible/crafting/station/shared_generic_tool.iff',
+											'object/tangible/survey_tool/shared_survey_tool_mineral.iff', 
+											'object/tangible/wearables/shirt/shared_shirt_s34.iff',
+											'object/tangible/wearables/pants/shared_pants_s29.iff',
+											'object/tangible/wearables/shoes/shared_shoes_s01.iff'
+										),
 	
-	'female:combat_brawler' : 		(	
-										'object/tangible/wearables/shirt/shared_shirt_s27.iff',
-										'object/tangible/wearables/pants/shared_pants_s07.iff',
-										'object/tangible/wearables/boots/shared_boots_s05.iff',
-										'object/tangible/wearables/gloves/shared_gloves_s03.iff'
-									),
+	'female:combat_brawler' : 			(	
+											'object/tangible/wearables/shirt/shared_shirt_s27.iff',
+											'object/tangible/wearables/pants/shared_pants_s07.iff',
+											'object/tangible/wearables/boots/shared_boots_s05.iff',
+											'object/tangible/wearables/gloves/shared_gloves_s03.iff'
+										),
 	
-	'male:combat_brawler' : 		(	
-										'object/tangible/wearables/shirt/shared_shirt_s14.iff',
-										'object/tangible/wearables/jacket/shared_jacket_s16.iff',
-										'object/tangible/wearables/pants/shared_pants_s14.iff',
-										'object/tangible/wearables/boots/shared_boots_s05.iff'
-									),
+	'male:combat_brawler' : 			(	
+											'object/tangible/wearables/shirt/shared_shirt_s14.iff',
+											'object/tangible/wearables/jacket/shared_jacket_s16.iff',
+											'object/tangible/wearables/pants/shared_pants_s14.iff',
+											'object/tangible/wearables/boots/shared_boots_s05.iff'
+										),
 	
-	'female:combat_marksman' : 		(	
-										'object/tangible/wearables/shirt/shared_shirt_s24.iff',
-										'object/tangible/wearables/pants/shared_pants_s24.iff',
-										'object/tangible/wearables/vest/shared_vest_s10.iff',
-										'object/tangible/wearables/shoes/shared_shoes_s02.iff'
-									),
+	'female:combat_marksman' : 			(	
+											'object/tangible/wearables/shirt/shared_shirt_s24.iff',
+											'object/tangible/wearables/pants/shared_pants_s24.iff',
+											'object/tangible/wearables/vest/shared_vest_s10.iff',
+											'object/tangible/wearables/shoes/shared_shoes_s02.iff'
+										),
 	
-	'male:combat_marksman' : 		(	
-										'object/tangible/wearables/jacket/shared_jacket_s05.iff',
-										'object/tangible/wearables/pants/shared_pants_s04.iff',
-										'object/tangible/wearables/boots/shared_boots_s21.iff'
-									)
-}
+	'male:combat_marksman' : 			(	
+											'object/tangible/wearables/jacket/shared_jacket_s05.iff',
+											'object/tangible/wearables/pants/shared_pants_s04.iff',
+											'object/tangible/wearables/boots/shared_boots_s21.iff'
+										),
+
+	'ithorian:female:social_entertainer' : (
+											'object/tangible/instrument/shared_slitherhorn.iff', 
+											'object/tangible/wearables/ithorian/shared_ith_pants_s11.iff',
+											'object/tangible/wearables/ithorian/shared_ith_shirt_s09.iff'
+										),
 	
-IthorianItems = {
-	'female:social_entertainer' : 	(	
-										'object/tangible/wearables/ithorian/shared_ith_pants_s11.iff',
-										'object/tangible/wearables/ithorian/shared_ith_shirt_s09.iff'
-									),
+	'ithorian:male:social_entertainer' : (	
+											'object/tangible/instrument/shared_slitherhorn.iff', 
+											'object/tangible/wearables/ithorian/shared_ith_pants_s08.iff',
+											'object/tangible/wearables/ithorian/shared_ith_shirt_s01.iff'
+										),
 	
-	'male:social_entertainer' : 	(	
-										'object/tangible/wearables/ithorian/shared_ith_pants_s08.iff',
-										'object/tangible/wearables/ithorian/shared_ith_shirt_s01.iff'
-									),
+	'ithorian:female:outdoors_scout' : 	(	
+											'object/tangible/crafting/station/shared_generic_tool.iff', 
+											'object/tangible/wearables/ithorian/shared_ith_shirt_s04.iff',
+											'object/tangible/wearables/ithorian/shared_ith_pants_s21.iff'
+										),
 	
-	'female:outdoors_scout' : 		(	
-										'object/tangible/wearables/ithorian/shared_ith_shirt_s04.iff',
-										'object/tangible/wearables/ithorian/shared_ith_pants_s21.iff'
-									),
+	'ithorian:male:outdoors_scout' : 	(
+											'object/tangible/crafting/station/shared_generic_tool.iff', 
+											'object/tangible/wearables/ithorian/shared_ith_shirt_s04.iff',
+											'object/tangible/wearables/ithorian/shared_ith_pants_s21.iff'
+										),
 	
-	'male:outdoors_scout' : 		(
-										'object/tangible/wearables/ithorian/shared_ith_shirt_s04.iff',
-										'object/tangible/wearables/ithorian/shared_ith_pants_s21.iff'
-									),
+	'ithorian:female:science_medic' : 	(
+											'object/tangible/crafting/station/shared_food_tool.iff',
+											'object/tangible/wearables/ithorian/shared_ith_shirt_s01.iff',
+											'object/tangible/wearables/ithorian/shared_ith_pants_s03.iff'
+										),
 	
-	'female:science_medic' : 		(
-										'object/tangible/wearables/ithorian/shared_ith_shirt_s01.iff',
-										'object/tangible/wearables/ithorian/shared_ith_pants_s03.iff'
-									),
+	'ithorian:male:science_medic' : 	(
+											'object/tangible/crafting/station/shared_food_tool.iff',
+											'object/tangible/wearables/ithorian/shared_ith_jacket_s02.iff',
+											'object/tangible/wearables/ithorian/shared_ith_shirt_s05.iff',
+											'object/tangible/wearables/ithorian/shared_ith_pants_s12.iff'
+										),
 	
-	'male:science_medic' : 			(
-										'object/tangible/wearables/ithorian/shared_ith_jacket_s02.iff',
-										'object/tangible/wearables/ithorian/shared_ith_shirt_s05.iff',
-										'object/tangible/wearables/ithorian/shared_ith_pants_s12.iff'
-									),
+	'ithorian:female:crafting_artisan' :(
+											'object/tangible/crafting/station/shared_generic_tool.iff',
+											'object/tangible/survey_tool/shared_survey_tool_mineral.iff', 
+											'object/tangible/wearables/ithorian/shared_ith_shirt_s05.iff',
+											'object/tangible/wearables/ithorian/shared_ith_pants_s06.iff',
+											'object/tangible/wearables/ithorian/shared_ith_vest_s02.iff'
+										),
 	
-	'female:crafting_artisan' : 	(
-										'object/tangible/wearables/ithorian/shared_ith_shirt_s05.iff',
-										'object/tangible/wearables/ithorian/shared_ith_pants_s06.iff',
-										'object/tangible/wearables/ithorian/shared_ith_vest_s02.iff'
-									),
+	'ithorian:male:crafting_artisan' : 	(
+											'object/tangible/crafting/station/shared_generic_tool.iff',
+											'object/tangible/survey_tool/shared_survey_tool_mineral.iff', 
+											'object/tangible/wearables/ithorian/shared_ith_vest_s02.iff',
+											'object/tangible/wearables/ithorian/shared_ith_shirt_s10.iff',
+											'object/tangible/wearables/ithorian/shared_ith_pants_s06.iff'
+										),
 	
-	'male:crafting_artisan' : 		(
-										'object/tangible/wearables/ithorian/shared_ith_vest_s02.iff',
-										'object/tangible/wearables/ithorian/shared_ith_shirt_s10.iff',
-										'object/tangible/wearables/ithorian/shared_ith_pants_s06.iff'
-									),
+	'ithorian:female:combat_brawler' : 	(
+											'object/tangible/wearables/ithorian/shared_ith_shirt_s02.iff',
+											'object/tangible/wearables/ithorian/shared_ith_pants_s07.iff',
+											'object/tangible/wearables/ithorian/shared_ith_gloves_s01.iff'
+										),
 	
-	'female:combat_brawler' : 		(
-										'object/tangible/wearables/ithorian/shared_ith_shirt_s02.iff',
-										'object/tangible/wearables/ithorian/shared_ith_pants_s07.iff',
-										'object/tangible/wearables/ithorian/shared_ith_gloves_s01.iff'
-									),
+	'ithorian:male:combat_brawler' : 	(
+											'object/tangible/wearables/ithorian/shared_ith_shirt_s02.iff',
+											'object/tangible/wearables/ithorian/shared_ith_jacket_s16.iff',
+											'object/tangible/wearables/ithorian/shared_ith_pants_s04.iff'
+										),
 	
-	'male:combat_brawler' : 		(
-										'object/tangible/wearables/ithorian/shared_ith_shirt_s02.iff',
-										'object/tangible/wearables/ithorian/shared_ith_jacket_s16.iff',
-										'object/tangible/wearables/ithorian/shared_ith_pants_s04.iff'
-									),
+	'ithorian:female:combat_marksman' : (
+											'object/tangible/wearables/ithorian/shared_ith_shirt_s05.iff',
+											'object/tangible/wearables/ithorian/shared_ith_pants_s06.iff',
+											'object/tangible/wearables/ithorian/shared_ith_vest_s02.iff'
+										),
 	
-	'female:combat_marksman' : 		(
-										'object/tangible/wearables/ithorian/shared_ith_shirt_s05.iff',
-										'object/tangible/wearables/ithorian/shared_ith_pants_s06.iff',
-										'object/tangible/wearables/ithorian/shared_ith_vest_s02.iff'
-									),
+	'ithorian:male:combat_marksman' : 	(
+											'object/tangible/wearables/ithorian/shared_ith_jacket_s05.iff',
+											'object/tangible/wearables/ithorian/shared_ith_shirt_s05.iff',
+											'object/tangible/wearables/ithorian/shared_ith_pants_s04.iff'
+										),
+										
+	'wookiee:female:crafting_artisan' : (
+											'object/tangible/crafting/station/shared_generic_tool.iff',
+											'object/tangible/survey_tool/shared_survey_tool_mineral.iff', 
+										),
+										
+	'wookiee:male:crafting_artisan' : (
+											'object/tangible/crafting/station/shared_generic_tool.iff',
+											'object/tangible/survey_tool/shared_survey_tool_mineral.iff', 
+										),
 	
-	'male:combat_marksman' : 		(
-										'object/tangible/wearables/ithorian/shared_ith_jacket_s05.iff',
-										'object/tangible/wearables/ithorian/shared_ith_shirt_s05.iff',
-										'object/tangible/wearables/ithorian/shared_ith_pants_s04.iff'
-									)
+	'wookiee:female:social_entertainer' : (
+											'object/tangible/instrument/shared_slitherhorn.iff', 
+										),
+										
+	'wookiee:male:social_entertainer' : (
+											'object/tangible/instrument/shared_slitherhorn.iff', 
+										),
+
+	'wookiee:female:science_medic' : 	(
+											'object/tangible/crafting/station/shared_food_tool.iff',
+										),
+										
+	'wookiee:male:science_medic' : 		(
+											'object/tangible/crafting/station/shared_food_tool.iff',
+										),
+	
+	'wookiee:female:outdoors_scout' : 	(
+											'object/tangible/crafting/station/shared_generic_tool.iff', 
+										),
+	'wookiee:male:outdoors_scout' : 	(
+											'object/tangible/crafting/station/shared_generic_tool.iff', 
+										),
+										
+	'wookiee:female:combat_brawler' : 	(),
+	'wookiee:male:combat_brawler' : 	(),
+	'wookiee:female:combat_marksman' : 	(),
+	'wookiee:male:combat_marksman' : 	(),
 }
 
 #All Get These
