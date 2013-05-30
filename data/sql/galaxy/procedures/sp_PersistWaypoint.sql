d1ad7e6c1a731ad6270d0f8b78ada1e83814c6cd
Eric Barr <ericbarrjr@me.com>
2013-05-30 14:39:26 -0700

    Persist now only updates existing waypoints

--- data/sql/galaxy/procedures/sp_PersistWaypoint.sql
+++ data/sql/galaxy/procedures/sp_PersistWaypoint.sql
@@ -6,7 +6,7 @@ DROP PROCEDURE IF EXISTS `sp_PersistWaypoint`;
 DELIMITER //
 CREATE PROCEDURE `sp_PersistWaypoint`(
     IN `in_object_id` bigint,
-    IN `in_player_id` bigint,
+    IN `in_owner_id` bigint,
     IN `in_x_coordinate` double,
     IN `in_y_coordinate` double,
     IN `in_z_coordinate` double,
@@ -15,32 +15,17 @@ CREATE PROCEDURE `sp_PersistWaypoint`(
     IN `in_name` varchar(255),
     IN `in_color` smallint(6))
 BEGIN
-    DECLARE does_exist INT;
 
-    select count(*) from waypoint w where w.id = in_object_id into does_exist;
-    if does_exist > 0 then
-        update waypoint w
-        set w.player_id = in_player_id,
-            w.x_coordinate = in_x_coordinate,
-            w.y_coordinate = in_y_coordinate,
-            w.z_coordinate = in_z_coordinate,
-            w.is_active = in_is_active,
-            w.planet = in_planet,
-            w.name = in_name,
-            w.color = in_color
-        where w.id = in_object_id;
-    else
-        insert into waypoint
-        set id = in_object_id,
-            player_id = in_player_id,
-            x_coordinate = in_x_coordinate,
-            y_coordinate = in_y_coordinate,
-            z_coordinate = in_z_coordinate,
-            is_active = in_is_active,
-            planet = in_planet,
-            name = in_name,
-            color = in_color;
-    end if;
+    update waypoint w
+    set w.owner_id = in_owner_id,
+        w.x_coordinate = in_x_coordinate,
+        w.y_coordinate = in_y_coordinate,
+        w.z_coordinate = in_z_coordinate,
+        w.is_active = in_is_active,
+        w.planet = in_planet,
+        w.name = in_name,
+        w.color = in_color
+    where w.id = in_object_id;
 
 END//
 DELIMITER ;
