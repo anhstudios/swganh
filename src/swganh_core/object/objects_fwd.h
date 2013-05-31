5290c3031d84ca038a66a027b614c5593bcb0f10
Eric Barr <ericbarrjr@me.com>
2013-05-31 05:30:30 -0700

    Added headers for easily including all the major object types or forward declaring them.

---
+++ src/swganh_core/object/objects_fwd.h
@@ -0,0 +1,28 @@
+// This file is part of SWGANH which is released under the MIT license.
+// See file LICENSE or go to http://swganh.com/LICENSE
+
+#pragma once
+
+namespace swganh {
+namespace object {
+
+class Building;
+class Cell;
+class Creature;
+class FactoryCrate;
+class Group;
+class Guild;
+class HarvesterInstallation;
+class Installation;
+class Intangible;
+class ManufactureSchematic;
+class Mission;
+class Player;
+class ResourceContainer;
+class Ship;
+class Static;
+class Tangible;
+class Waypoint;
+class Weapon;
+
+}}  // namespace swganh::object
