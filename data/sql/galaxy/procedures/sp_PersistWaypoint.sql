473339a454e0c3397d4ac8820f5ba9a522488244
Eric Barr <ericbarrjr@me.com>
2013-05-31 14:50:19 -0700

    Updated query to insert if new and update if existing

--- data/sql/galaxy/procedures/sp_PersistWaypoint.sql
+++ data/sql/galaxy/procedures/sp_PersistWaypoint.sql
@@ -15,18 +15,29 @@ CREATE PROCEDURE `sp_PersistWaypoint`(
     IN `in_name` varchar(255),
     IN `in_color` smallint(6))
 BEGIN
+    DECLARE does_exist INT;
 
-    update waypoint w
-    set w.owner_id = in_owner_id,
-        w.x_coordinate = in_x_coordinate,
-        w.y_coordinate = in_y_coordinate,
-        w.z_coordinate = in_z_coordinate,
-        w.is_active = in_is_active,
-        w.planet = in_planet,
-        w.name = in_name,
-        w.color = in_color
-    where w.id = in_object_id;
-
+    select count(*) from waypoint w where w.id = in_object_id into does_exist;
+    if does_exist > 0 then
+        update waypoint w
+        set w.owner_id = in_owner_id,
+            w.x_coordinate = in_x_coordinate,
+            w.y_coordinate = in_y_coordinate,
+            w.z_coordinate = in_z_coordinate,
+            w.is_active = in_is_active,
+            w.planet = in_planet,
+            w.name = in_name,
+            w.color = in_color
+        where w.id = in_object_id;
+    else
+        insert into waypoint (id, owner_id, x_coordinate,
+            y_coordinate, z_coordinate, is_active,
+            planet, name, color)
+        values (in_object_id,
+            in_owner_id, in_x_coordinate, in_y_coordinate,
+            in_z_coordinate, in_is_active, in_planet,
+            in_name, in_color);
+    end if;
 END//
 DELIMITER ;
 /*!40014 SET FOREIGN_KEY_CHECKS=1 */;
